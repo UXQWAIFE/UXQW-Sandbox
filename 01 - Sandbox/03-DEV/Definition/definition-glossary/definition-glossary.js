1a64c2cca2693f5029229e2b65e851a0e6d88357
UXQWAIFE <ux.aife@finances.gouv.fr>
2023-11-15 17:02:57 +0100

    Edit Définition - à Corrigé

--- 01 - Sandbox/03-DEV/Definition/definition-glossary/definition-glossary.js
+++ 01 - Sandbox/03-DEV/Definition/definition-glossary/definition-glossary.js
@@ -4,8 +4,9 @@ let wordsToDefineEl = document.querySelectorAll('[data-def]');
 window.addEventListener('load', (event) => {
 	wordsToDefineEl.forEach(el => {
 		const defID = `def-${el.innerHTML}`;
+		// définir la const pour choper l'url à injecter avec un : const urlFrom = "%RB%urlGet"
 		el.setAttribute('data-def', el.innerHTML);
-		el.setAttribute('href', `./glossary.html#${defID}`);
+		el.setAttribute('href', `./glossary.html#${defID}`); // ./glossary.html#${defID}{urlFrom}
 		el.setAttribute('title', 'Accèder à la définition');
 		el.classList.add('rcn-definition__link');
 	});
