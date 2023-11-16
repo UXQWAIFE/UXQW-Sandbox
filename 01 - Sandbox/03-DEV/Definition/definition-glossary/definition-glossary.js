33b48c47c375d285ec2394be052ecd51435e04bb
emilie <emiliemorassi@gmail.com>
2023-11-16 11:00:20 +0100

    definition-glossary dynamic return back link

--- 01 - Sandbox/03-DEV/Definition/definition-glossary/definition-glossary.js
+++ 01 - Sandbox/03-DEV/Definition/definition-glossary/definition-glossary.js
@@ -1,14 +1,24 @@
 let wordsToDefineEl = document.querySelectorAll('[data-def]');
 
-
 window.addEventListener('load', (event) => {
 	wordsToDefineEl.forEach(el => {
-		const defID = `def-${el.innerHTML}`;
-		// définir la const pour choper l'url à injecter avec un : const urlFrom = "%RB%urlGet"
-		el.setAttribute('data-def', el.innerHTML);
-		el.setAttribute('href', `./glossary.html#${defID}`); // ./glossary.html#${defID}{urlFrom}
+		const defID = `def-${el.innerHTML.toLowerCase()}`;
+		el.setAttribute('data-def', el.innerHTML.toLowerCase());
+		el.setAttribute('href', `./glossary.html#${defID}`);
+		el.setAttribute('id', defID);
 		el.setAttribute('title', 'Accèder à la définition');
 		el.classList.add('rcn-definition__link');
 	});
 });
 
+//if on glossary page, create Go Back Link.
+if((window.location.href).includes('glossary.html#') && !(window.location.href).includes('definition-glossary.html')){
+	var id = window.location.toString().split('#')[1];
+	let currentDef = document.querySelector(`#${id}`);
+	let goBackLink = document.createElement('a');
+	goBackLink.setAttribute('href', `./definition-glossary.html#${id}`);
+	goBackLink.setAttribute('title', 'Retourner à la lecture');
+	goBackLink.classList.add('rcn-definition__link');
+	goBackLink.innerHTML = '  Retourner à la lecture ';
+	currentDef.appendChild(goBackLink);
+}
